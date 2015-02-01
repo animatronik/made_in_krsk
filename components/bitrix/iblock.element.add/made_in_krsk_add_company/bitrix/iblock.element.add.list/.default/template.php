c7a9f1bf74b6c65904d20b45fbc0e3ed650bd61b
Denis Spiridonov <ianimatronik@gmail.com>
2015-02-01 18:41:01 +0700

    Revert "Revert "Template of (/personal/company/)""
    This reverts commit 302f342b1ad3d4982e7c38d0e5497e76b7e443ee.

--- components/bitrix/iblock.element.add/made_in_krsk_add_company/bitrix/iblock.element.add.list/.default/template.php
+++ components/bitrix/iblock.element.add/made_in_krsk_add_company/bitrix/iblock.element.add.list/.default/template.php
@@ -7,9 +7,6 @@ $colspan = 2;
 if ($arResult["CAN_EDIT"] == "Y") $colspan++;
 if ($arResult["CAN_DELETE"] == "Y") $colspan++;
 ?>
-<?if (strlen($arResult["MESSAGE"]) > 0):?>
-	<?=ShowNote($arResult["MESSAGE"])?>
-<?endif?>
 
 <h2><?=GetMessage("IBLOCK_ADD_LIST_TITLE")?></h2>
 
@@ -19,13 +16,13 @@ if ($arResult["CAN_DELETE"] == "Y") $colspan++;
 			<div class="firm-edit">
 				<?if ($arResult["CAN_EDIT"] == "Y"):?>
 					<?if ($arElement["CAN_EDIT"] == "Y"):?>
-						<a href="<?=$arParams["EDIT_URL"]?>?edit=Y&amp;CODE=<?=$arElement["ID"]?>"><?=GetMessage("IBLOCK_ADD_LIST_EDIT")?></a>
+						<a class="bt_blue big" href="<?=$arParams["EDIT_URL"]?>?edit=Y&amp;CODE=<?=$arElement["ID"]?>"><?=GetMessage("IBLOCK_ADD_LIST_EDIT")?></a>
 						<?else:?>&nbsp;
 					<?endif?>
 				<?endif?>
 				<?if ($arResult["CAN_DELETE"] == "Y"):?>
 					<?if ($arElement["CAN_DELETE"] == "Y"):?>
-						<a href="?delete=Y&amp;CODE=<?=$arElement["ID"]?>&amp;<?=bitrix_sessid_get()?>" onClick="return confirm('<?echo CUtil::JSEscape(str_replace("#ELEMENT_NAME#", $arElement["NAME"], GetMessage("IBLOCK_ADD_LIST_DELETE_CONFIRM")))?>')"><?=GetMessage("IBLOCK_ADD_LIST_DELETE")?></a>
+						<a class="bt_blue big" href="?delete=Y&amp;CODE=<?=$arElement["ID"]?>&amp;<?=bitrix_sessid_get()?>" onClick="return confirm('<?echo CUtil::JSEscape(str_replace("#ELEMENT_NAME#", $arElement["NAME"], GetMessage("IBLOCK_ADD_LIST_DELETE_CONFIRM")))?>')"><?=GetMessage("IBLOCK_ADD_LIST_DELETE")?></a>
 						<?else:?>&nbsp;
 					<?endif?>
 				<?endif?>
@@ -33,19 +30,17 @@ if ($arResult["CAN_DELETE"] == "Y") $colspan++;
 
 			<div class="firm-head">
 				<h3><?=$arElement["NAME"]?></h3>
-				<span class="firm-logo">
-					<?if ($arElement["PREVIEW_PICTURE"]):?>
-						<img src="<?=CFile::GetPath($arElement["PREVIEW_PICTURE"])?>" width="200">
-					<?endif?>
-				</span>
+				<?if ($arElement["PREVIEW_PICTURE"]):?>
+					<span class="firm-logo">					
+						<img src="<?=CFile::GetPath($arElement["PREVIEW_PICTURE"])?>" width="200">					
+					</span>
+				<?endif?>								
 			</div>
 
 			<div class="firm-info">
-				<?=$arElement["PREVIEW_TEXT"];?>
+				<?=htmlspecialcharsBack($arElement["PREVIEW_TEXT"]);?>
 			</div>
-		<?endforeach?>
-	<?else:?>		
-		<?=GetMessage("IBLOCK_ADD_LIST_EMPTY")?>		
+		<?endforeach?>				
 	<?endif?>
 <?endif?>
 <div class="add-element">
